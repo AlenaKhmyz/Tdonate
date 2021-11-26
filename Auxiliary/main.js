fc7f006b7c20914b9dfdd5921b4a10f76168e20c
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-11-27 00:41:33 +0300

    modified auxiliary

--- Auxiliary/main.js
+++ Auxiliary/main.js
@@ -1,79 +1,98 @@
-const WIDTH = 740
-const HEIGHT = 160
-const PADDING = 10
-const DPI_WIDTH = WIDTH * 2
-const DPI_HEIGHT = HEIGHT * 2
-const VIEW_HEIGHT = DPI_HEIGHT - PADDING * 2
-const ROWS_COUNT = 5
-const CIRCLE_RADIUS = 20
 
-function chart(canvas, data) {
-  const ctx = canvas.getContext('2d')
-  canvas.style.width = WIDTH + 'px'
-  canvas.style.height = HEIGHT + 'px'
-  canvas.width =  DPI_WIDTH
-  canvas.height = DPI_HEIGHT
-
- const [yMin, yMax] =  computerBoundaries(data)
- 
-
-  // === y axis
-  const step = VIEW_HEIGHT / ROWS_COUNT
-  const textStep = (yMax - yMin) / ROWS_COUNT
-  console.log(textStep);
-
-  ctx.beginPath()
-  ctx.strokeStyle="#EFF3F9"
-  ctx.font = " normal 20px Lato "
-  ctx.fillStyle = "#4F4F4F"
-  
-  for (let i = 1; i <= ROWS_COUNT; i++ ) {
-    const y = step * i
-    const text = yMax - textStep * i
-    ctx.fillText(text.toString(), 5, y + PADDING - 5)
-    ctx.moveTo(0, y + PADDING);
-    ctx.lineTo(DPI_WIDTH, y + PADDING)
- 
-  }
-  ctx.stroke()
-  ctx.closePath()
-  //===
-
-  ctx.beginPath()
-  ctx.lineWidth = 4
-  ctx.lineJoin = 'round'
-  ctx.strokeStyle = '#1A76C8'
-  for ( const [x, y] of data) {
-    ctx.lineTo(x, DPI_HEIGHT - PADDING - y)
-  }
- 
-  ctx.stroke()
-  ctx.closePath()
-
-}
-
-chart(document.getElementById('chart'), [
-  [0, 55],
-  [211, 125],
-  [422, 50],
-  [633, 240],
-  [844, 60],
-  [1055, 120],
-  [1266, 225],
-  [1477, 70]
-])
-
-function computerBoundaries(data) {
-  let min 
-  let max 
-
-  for (const [, y] of data) {
-    if(typeof min !== 'number') min = y
-    if(typeof max !== 'number') max = y
-
-    if (min > y) min = y
-    if (max < y) max = y
-  }
+    //------------------------1. PREPARATION-------------------------//
+    //-----------------------------SVG-------------------------------// 
     
-  return [min, max]
-}
+    const width = 820;
+    const height = 180;
+    const margin = 5;
+    const padding = 5;
+    const adj = 30;
+     // we are appending SVG first 
+    const svg = d3.select("#chart_div").append("svg")
+      .attr("preserveAspectRatio", "xMinYMin meet")
+      .attr("viewBox", "-"
+        + adj + " -"
+        + adj + " "
+        + (width + adj * 3) + " "
+        + (height + adj * 3))
+      .style("padding", padding)
+      .style("margin", margin)
+      .classed("svg-content", true);
+
+    const timeConv = d3.timeParse("%d-%b-%Y");
+ //-----------------------------DATA------------------------------// 
+    const data = [
+      { x: dayjs("2021-01-31T00:00:00.000").valueOf(), y: 0.7 },
+      { x: dayjs("2021-02-01T00:00:00.000").valueOf(), y: 1.7 },
+      { x: dayjs("2021-02-02T00:00:00.000").valueOf(), y: 0.5 },
+      { x: dayjs("2021-02-03T00:00:00.000").valueOf(), y: 3 },
+      { x: dayjs("2021-02-04T00:00:00.000").valueOf(), y: 0.75 },
+      { x: dayjs("2021-02-05T00:00:00.000").valueOf(), y: 1.5 },
+      { x: dayjs("2021-02-06T00:00:00.000").valueOf(), y: 2.7 },
+      { x: dayjs("2021-02-07T00:00:00.000").valueOf(), y: 1.2 },
+    ];
+
+     //----------------------------SCALES-----------------------------// 
+
+    const xScale = d3.scaleTime().range([0, width]);
+    const yScale = d3.scaleLinear().rangeRound([height, 0]);
+    
+    xScale.domain(d3.extent(data, d => d.x));
+    yScale.domain([0, d3.max(data, d => d.y)]);
+
+     //-----------------------------AXES------------------------------// 
+
+    const yaxis = d3.axisLeft()
+       //.ticks(20, "s") 
+      .tickValues([0, 0.75, 1.5, 2.25, 3])
+      .tickFormat(v => v )
+      .tickPadding(10)
+      .tickSize(-width)
+      .scale(yScale);
+
+    const xaxis = d3.axisBottom()
+      //.ticks(d3.timeDay.every(1)) 
+      //.ticks(data.length)
+      .tickValues(data.map(point => point.x))
+      .tickFormat((v) => dayjs(v).format("DD/MM"))
+      .tickSize(-height)
+      .tickPadding(10)
+      .scale(xScale);
+
+    svg.append("g")
+        .attr("class", "axis axis-x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xaxis);
+    
+    svg.append("g")
+        .attr("class", "axis axis-y")
+        .call(yaxis)
+        .selectAll("g.axis.axis-y > g.tick")
+        .classed("unactive", (d, i) => i % 2 !== 0)
+
+     //----------------------------LINES------------------------------// 
+
+    const line = d3.line()
+      .x(d => xScale(d.x))
+      .y(d => yScale(d.y))
+      .curve(d3.curveMonotoneX)
+    (data);
+
+    svg.append("g")
+      .attr("class", "lines")
+      .append("path")
+      .attr("d", line);
+    
+    svg.append("g")
+      .attr("class", "points")
+      .selectAll("g.points > circle.point")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("class", "point")
+      .attr("cx", d => xScale(d.x))
+      .attr("cy", d => yScale(d.y))
+      .attr("r", 3)
+
+      
+    
